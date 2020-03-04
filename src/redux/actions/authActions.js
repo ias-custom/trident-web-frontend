49883d8336842221ef138fbaedf087df2414361b
Luigui Saenz <luigui.bless95@gmail.com>
2020-03-04 14:28:11 -0500

    fix get permissions of user in login

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -6,8 +6,8 @@ import {
   REFRESH_TOKEN,
   GET_CUSTOMERS,
   GET_CUSTOMER_SELECTED
-} from '../actionTypes';
-import AuthService from '../../services/AuthService';
+} from "../actionTypes";
+import AuthService from "../../services/AuthService";
 
 const service = new AuthService();
 
@@ -20,78 +20,103 @@ export const refreshToken = () => {
       if (response.status === 200) {
         const data = { ...auth, token: response.data.token };
 
-        localStorage.setItem('auth', JSON.stringify(data));
+        localStorage.setItem("auth", JSON.stringify(data));
         dispatch({ type: REFRESH_TOKEN, payload: data });
 
-        console.log('Refresh token');
+        console.log("Refresh token");
       } else {
         dispatch(logout());
-        console.log('Error refresh token', response.data);
+        console.log("Error refresh token", response.data);
       }
 
       return response;
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 };
 
 export const login = (username, password) => {
-  return async (dispatch) => {
-    dispatch({ type: ON_LOADING, loading: true});
+  return async dispatch => {
+    dispatch({ type: ON_LOADING, loading: true });
 
     try {
       const response = await service.login(username, password);
       if (response.status === 200) {
-        const { id, first_name = '', last_name = '', username, token, customers, is_superuser, roles } = response.data;
-        dispatch({ type: GET_CUSTOMERS, payload: customers })
-        dispatch({ type: GET_CUSTOMER_SELECTED, payload: customers[0].id })
-        localStorage.setItem('customers',  JSON.stringify(customers));
-        localStorage.setItem('customerSelectedId',  JSON.stringify(customers[0].id));
+        const {
+          id,
+          first_name = "",
+          last_name = "",
+          username,
+          token,
+          customers,
+          is_superuser,
+          roles
+        } = response.data;
+        dispatch({ type: GET_CUSTOMERS, payload: customers });
+        dispatch({ type: GET_CUSTOMER_SELECTED, payload: customers[0].id });
+        localStorage.setItem("customers", JSON.stringify(customers));
+        localStorage.setItem(
+          "customerSelectedId",
+          JSON.stringify(customers[0].id)
+        );
         const fullName = first_name ? `${first_name} ${last_name}` : username;
-        const avatar = fullName.replace(/\s/g, '').substr(0, 2).toUpperCase();
-        const permissionsList = roles.map( ({codename}) => codename)
-        const auth = { id, fullName, username, avatar, token, is_superuser, permissions:permissionsList };
+        const avatar = fullName
+          .replace(/\s/g, "")
+          .substr(0, 2)
+          .toUpperCase();
+        const permissionsList = roles[0].permissions.map(
+          ({ codename }) => codename
+        );
+        const auth = {
+          id,
+          fullName,
+          username,
+          avatar,
+          token,
+          is_superuser,
+          permissions: permissionsList
+        };
 
-        localStorage.setItem('auth', JSON.stringify(auth));
+        localStorage.setItem("auth", JSON.stringify(auth));
 
-        dispatch({ type: LOGIN, payload: auth});
+        dispatch({ type: LOGIN, payload: auth });
       } else {
-        dispatch({ type: LOGIN_ERROR})
+        dispatch({ type: LOGIN_ERROR });
       }
 
       return response;
     } catch (error) {
       console.error(error);
     } finally {
-      dispatch({ type: ON_LOADING, loading: false});
+      dispatch({ type: ON_LOADING, loading: false });
     }
-  }
+  };
 };
 
 export const logout = () => {
-  localStorage.removeItem('auth');
-  localStorage.removeItem('customers')
+  localStorage.removeItem("auth");
+  localStorage.removeItem("customers");
 
   return { type: LOGOUT };
 };
 
-export const forgotPassword = (email) => {
+export const forgotPassword = email => {
   return async dispatch => {
     try {
       return await service.forgotPassword(email);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 };
 
-export const resetPassword = (form) => {
+export const resetPassword = form => {
   return async dispatch => {
     try {
       return await service.resetPassword(form);
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 };
